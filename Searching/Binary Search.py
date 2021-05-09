d46c6ff2c7a31732dc9e2d687a30815105b38ada
Abhishek Negi <abhishek96negi@gmail.com>
2021-05-09 12:38:10 +0530

    binary_search

---
+++ Searching/Binary Search.py
@@ -0,0 +1,24 @@
+pos= -1
+
+def search(list,n):
+
+    l=0
+    u=len(list)-1
+
+    while l<=u:
+        mid=(l+u)//2
+
+        if list[mid]==n:
+            globals()['pos']=mid
+            return True
+        else:
+            if list[mid]<n:
+                l=mid+1
+            else:
+                u=mid-1
+list=[4,7,8,12,45,99]
+n=45
+if search(list,n):
+    print("Found at",pos+1)
+else:
+    print("Not Found")
