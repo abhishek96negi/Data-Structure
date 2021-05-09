78bb89a3ad5b9649cdbe1c5c1b2084510adbbdef
Abhishek Negi <abhishek96negi@gmail.com>
2021-05-09 12:42:28 +0530

    bubble_sort

---
+++ Sorting/Bubble Sort.py
@@ -0,0 +1,13 @@
+
+def sort(nums):
+    for i in range(len(nums)-1,0,-1):
+        for j in range(i):
+            if nums[j]>nums[j+1]:
+                nums[j],nums[j+1]=nums[j+1],nums[j]
+
+
+
+nums=[5,3,8,6,7,2]
+print("Before Swapping :",nums,sep='\n')
+sort(nums)
+print("After Swapping :",nums,sep='\n')
